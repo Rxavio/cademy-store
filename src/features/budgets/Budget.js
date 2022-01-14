253be8253452bca59424c4dea26ec007d7ac4e76
xavioo <rucahoxavi@gmail.com>
2022-01-14 07:01:59 +0200

    Budget file

--- src/features/budgets/Budget.js
+++ src/features/budgets/Budget.js
@@ -7,9 +7,10 @@ const Transactions = () => {
   const budgets = useSelector(selectBudgets);
   return (
     <ul className='comments-container'>
-      { budgets.map(budget => <Budget budget={budget}/>) }
+      { budgets.map(budget => <Budget budget={budget} key={budget.category}/>) }
     </ul>
   );
 };
 
 export default Transactions;
+
