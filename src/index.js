64a83d6d7c731d0c89e5ea5cb61855b85c4a3534
xavioo <rucahoxavi@gmail.com>
2022-01-14 06:32:01 +0200

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import App from './app/App';
 import { Provider } from 'react-redux';
-
-import { App } from './app/App.js';
-import { store } from './app/store.js';
+import store from './app/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
